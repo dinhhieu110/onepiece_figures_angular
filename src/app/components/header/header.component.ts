ac15ba9220bbde7f3958f9d99846be4919730d5f
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-05-15 20:14:51 +0700

    [EA-04] Build header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '../custom/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  title = signal<string>('My App');
+  greeting() {
+    console.log('Hello Wilson!!!');
+  }
 }
